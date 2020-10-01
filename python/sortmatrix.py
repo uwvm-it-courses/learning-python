5d9936587461f87aa9c8b7b4cde91b6cb5ffc6f7
EmmaSamoilova <emmasamojlova@gamil.com>
2020-10-01 18:15:39 +0300

    added sort matrix

---
+++ python/sortmatrix.py
@@ -0,0 +1,72 @@
+def matrixrandom8x10 ():
+    from random import random
+    N= 8
+    M= 10
+    a =[]
+    for i in range (N):
+        z =[]
+        for j in range (M):
+            n = int(random()*100)
+            z.append(n)
+            print(n, end = ' ')
+        print()
+        a.append(z)
+    print()
+    b=[]
+    i=0
+    while len(b) < N*M:
+        b += a[i]
+        i+=1
+    print(b)
+    for i in range(len(b)):
+        for j in range(len(b)-1):
+            if b[j] > b[j + 1]:
+                buff = b[j]
+                b[j] = b[j + 1]
+                b[j + 1] = buff
+    print(b)
+    g = []
+    for i in range (N):
+        h =[]
+        for j in range (M):
+            h.append(b[i*N + j])
+        print(h, end = ' ')
+        print()
+        g.append(h)
+    print()
+
+def matrixrandom2x3 ():
+    from random import random
+    N= 2
+    M= 3
+    a =[]
+    for i in range (N):
+        z =[]
+        for j in range (M):
+            n = int(random()*100)
+            z.append(n)
+            print(n, end = ' ')
+        print()
+        a.append(z)
+    print()
+    b = a[0] + a[1] 
+    print(b)
+    for i in range(len(b)):
+        for j in range(len(b)-1):
+            if b[j] > b[j + 1]:
+                buff = b[j]
+                b[j] = b[j + 1]
+                b[j + 1] = buff
+    print(b)
+    g = []
+    for i in range (N):
+        h =[]
+        for j in range (M):
+            h.append(b[i*N + j])
+        print(h, end = ' ')
+        print()
+        g.append(h)
+    print()
+    
+matrixrandom8x10 ()
+matrixrandom2x3()
