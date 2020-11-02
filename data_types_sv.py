db07b256ff41f29b915b5f6d04748976be23622e
21sv <skhromaliuk@gmail.com>
2020-11-02 19:52:29 +0200

    Зміни до data_types.py

---
+++ data_types_sv.py
@@ -0,0 +1,103 @@
+x = "Hello World"
+print(type(x), x)
+	
+x = 20
+print(type(x), x)
+	
+x = 20.5
+print(type(x), x)
+	
+x = 1j
+print(type(x), x)
+
+x = ["apple", "banana", "cherry"]
+print(type(x), x)
+
+x = ("apple", "banana", "cherry")
+print(type(x), x)
+
+x = range(6)
+print(type(x), x)
+	
+x = {"name" : "John", "age" : 36, "family" : [{"name" : "Ivasya", "age" : 36}, {"name": "Ivasya", "age" : 36}, {"name" : "Ivasya", "age" : 36}]}
+print(type(x), x)
+	
+x = {"apple", "banana", "cherry"}
+print(type(x), x)
+	
+x = frozenset({"apple", "banana", "cherry"})
+print(type(x), x)
+	
+x = True
+print(type(x), x)
+	
+x = False
+print(type(x), x)
+	
+x = b"Hello"
+print(type(x), x)
+	
+x = bytearray(5)
+print(type(x), x)
+	
+x = memoryview(bytes(5))
+print(type(x), x)
+	
+print("n\###################################\n")
+
+print("########################Explicit types######################")
+
+print( "n\#################################\n")
+
+
+
+x = str("Hello world")
+print(type(x), x)
+	
+x = int(20)
+print(type(x), x)
+	
+x = float(20.5)
+print(type(x), x)
+	
+x = complex(1j)
+print(type(x), x)
+	
+x = list(("apple", "banana", "cherry"))
+print(type(x), x)
+	
+x = tuple(("apple", "banana", "cherry"))
+print(type(x), x)
+	
+x = range(6)
+print(type(x), x)
+	
+x = dict(name = "Jhon", age = 36)
+print(type(x), x)
+	
+x = set(("apple", "banana", "cherry"))
+print(type(x), x)
+	
+x = frozenset(("apple", "banana", "cherry"))
+print(type(x), x)
+	
+x = bool(5)
+print(type(x), x)
+	
+x = bool(1)
+print(type(x), x)
+	
+x = bool(-1)
+print(type(x), x)
+	
+x = bool(0)
+print(type(x), x)
+	
+x = bytes(5)
+print(type(x), x)
+	
+x = bytearray(5)
+print(type(x), x)
+	
+x = memoryview(bytes(5))
+print(type(x), x)
